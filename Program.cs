cd724a2fb3a6fd743ca8582b37efa856519ac5f7
Jahle <Jahle@ABLACKMAN>
2024-05-20 15:29:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int year1 = 2016;
+        int year2 = 2018;
+
+        Console.WriteLine("Is " + year1 + " a leap year? " + IsLeapYear(year1));
+        Console.WriteLine("Is " + year2 + " a leap year? " + IsLeapYear(year2));
+    }
+
+    static bool IsLeapYear(int year)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
